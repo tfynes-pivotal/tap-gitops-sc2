e6510cb59a62b7d6d455a2205434e50a8c782677
Mark Fynes <tfynes@vmware.com>
2023-06-21 14:25:35 -0400

    harbor12345

--- clusters/taplab/cluster-config/sensitive-file-structures/create_user-registry-dockerconfig-secret.sh
+++ clusters/taplab/cluster-config/sensitive-file-structures/create_user-registry-dockerconfig-secret.sh
@@ -2,4 +2,4 @@ kubectl -n tap-install create secret docker-registry \
 --dry-run=client user-registry-dockerconfig \
 --docker-server 'http://myrepo-harbor.harbor.svc.cluster.local' \
 --docker-username=admin \
---docker-password='Q7zY8kAnVM' -o yaml  > user-registry-dockerconfig.yaml
+--docker-password='Harbor12345' -o yaml  > user-registry-dockerconfig.yaml
