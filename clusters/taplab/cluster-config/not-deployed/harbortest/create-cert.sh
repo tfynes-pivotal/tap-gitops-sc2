d4570ba1098d4084557a98b3b2e909db8a84ff41
Mark Fynes <tfynes@vmware.com>
2023-06-21 14:11:25 -0400

    experimenting with helm local harbor with private cert

--- clusters/taplab/cluster-config/not-deployed/harbortest/create-cert.sh
+++ clusters/taplab/cluster-config/not-deployed/harbortest/create-cert.sh
@@ -1,1 +1,10 @@
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout harborcert.key -addext "subjectAltName = DNS:myrepo-harbor.harbor.svc.cluster.local" -x509 -days 365 -out harborcert.crt
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout tls.key -addext "subjectAltName = DNS:myrepo-harbor.harbor.svc.cluster.local" -x509 -days 365 -out tls.crt
+kubectl -n harbor delete secret harborcerts
+kubectl -n harbor create secret generic harborcerts --from-file=./tls.crt --from-file=tls.key
+
+
+#helm pull harbor/harbor
+#tar xvf ./harbor-1.12.2.tgz
+#cd harbor/
+#helm -n harbor delete myrepo
+#helm -n harbor install myrepo .
