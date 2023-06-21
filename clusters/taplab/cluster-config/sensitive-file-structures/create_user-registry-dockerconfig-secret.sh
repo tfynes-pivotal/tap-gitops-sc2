85b47332c740311837539731882b7f7af9211c7e
Mark Fynes <tfynes@vmware.com>
2023-06-21 11:04:41 -0400

    .user-registry-dockerconfig insecure for harbor

--- clusters/taplab/cluster-config/sensitive-file-structures/create_user-registry-dockerconfig-secret.sh
+++ clusters/taplab/cluster-config/sensitive-file-structures/create_user-registry-dockerconfig-secret.sh
@@ -1,5 +1,5 @@
 kubectl -n tap-install create secret docker-registry \
 --dry-run=client user-registry-dockerconfig \
---docker-server 'https://index.docker.io/v1/' \
---docker-username=<userid> \
---docker-password='<password>' -o yaml  > user-registry-dockerconfig.yaml
+--docker-server 'http://myrepo-harbor.harbor.svc.cluster.local' \
+--docker-username=admin \
+--docker-password='Q7zY8kAnVM' -o yaml  > user-registry-dockerconfig.yaml
