fca56f5b243b096c953f38051a85a6ca443658a9
Mark Fynes <tfynes@vmware.com>
2023-06-22 09:19:23 -0400

    .

--- clusters/taplab/cluster-config/not-deployed/harbortest/create-cert.sh
+++ clusters/taplab/cluster-config/not-deployed/harbortest/create-cert.sh
@@ -1,12 +1,11 @@
 
-openssl genrsa -out ca.key 4096
+openssl genrsa -out tls.key 4096
 openssl req -x509 -new -nodes -sha512 -days 3650 \
  -subj "/C=CN/ST=Boston/L=Boston/O=example/OU=Personal/CN=myrepo-harbor.harbor.svc.cluster.local" \
- -key ca.key \
- -out ca.crt
+ -key tls.key 
 
 kubectl -n harbor delete secret harborcerts
-kubectl -n harbor create secret generic harborcerts --from-file=./ca.crt --from-file=ca.key
+kubectl -n harbor create secret generic harborcerts --from-file=./tls.crt --from-file=tls.key
 
 #openssl req -newkey rsa:4096 -nodes -sha256 -keyout tls.key -addext "subjectAltName = DNS:myrepo-harbor.harbor.svc.cluster.local" -x509 -days 365 -out tls.crt
 #kubectl -n harbor delete secret harborcerts
