0675c3be2718677fd80bcf2061195328a79a6821
Mark Fynes <tfynes@vmware.com>
2023-06-23 11:24:39 -0400

    updated ca in tap-non-sensitive values

--- clusters/taplab/cluster-config/not-deployed/harbortest/create-cert.sh
+++ clusters/taplab/cluster-config/not-deployed/harbortest/create-cert.sh
@@ -1,15 +1,13 @@
 
-openssl genrsa -out tls.key 4096
-openssl req -x509 -new -nodes -sha512 -days 3650 \
- -subj "/C=CN/ST=Boston/L=Boston/O=example/OU=Personal/CN=myrepo-harbor.harbor.svc.cluster.local" \
- -key tls.key 
+#openssl genrsa -out tls.key 4096
+#openssl req -x509 -new -nodes -sha512 -days 3650 -subj "/C=CN/ST=Boston/L=Boston/O=example/OU=Personal/CN=myrepo-harbor.harbor.svc.cluster.local" -key tls.key 
 
-kubectl -n harbor delete secret harborcerts
-kubectl -n harbor create secret generic harborcerts --from-file=./tls.crt --from-file=tls.key
-
-#openssl req -newkey rsa:4096 -nodes -sha256 -keyout tls.key -addext "subjectAltName = DNS:myrepo-harbor.harbor.svc.cluster.local" -x509 -days 365 -out tls.crt
 #kubectl -n harbor delete secret harborcerts
 #kubectl -n harbor create secret generic harborcerts --from-file=./tls.crt --from-file=tls.key
+
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout tls.key -addext "subjectAltName = DNS:myrepo-harbor.harbor.svc.cluster.local" -x509 -days 365 -out tls.crt
+kubectl -n harbor delete secret harborcerts
+kubectl -n harbor create secret generic harborcerts --from-file=./tls.crt --from-file=tls.key
 
 
 #helm pull harbor/harbor
