d58e6a9e00b7b12667ca49b5e614eea11a2bad23
Mark Fynes <tfynes@vmware.com>
2023-06-23 14:27:13 -0400

    updated domain for harbor to be externally resolved

--- clusters/taplab/cluster-config/not-deployed/harbortest/create-cert.sh
+++ clusters/taplab/cluster-config/not-deployed/harbortest/create-cert.sh
@@ -5,10 +5,14 @@
 #kubectl -n harbor delete secret harborcerts
 #kubectl -n harbor create secret generic harborcerts --from-file=./tls.crt --from-file=tls.key
 
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout tls.key -addext "subjectAltName = DNS:myrepo-harbor.harbor.svc.cluster.local" -x509 -days 365 -out tls.crt
+# this cert allowed tap to install
+#openssl req -newkey rsa:4096 -nodes -sha256 -keyout tls.key -addext "subjectAltName = DNS:myrepo-harbor.akslab1.tap.fynesy.com" -x509 -days 365 -out tls.crt
+#kubectl -n harbor delete secret harborcerts
+#kubectl -n harbor create secret generic harborcerts --from-file=./tls.crt --from-file=tls.key
+
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout tls.key -addext "subjectAltName = DNS:myrepo-harbor.akslab1.tap.fynesy.com" -x509 -days 365 -out tls.crt
 kubectl -n harbor delete secret harborcerts
 kubectl -n harbor create secret generic harborcerts --from-file=./tls.crt --from-file=tls.key
-
 
 #helm pull harbor/harbor
 #tar xvf ./harbor-1.12.2.tgz
--- clusters/taplab/cluster-config/sensitive-file-structures/create_user-registry-dockerconfig-secret.sh
+++ clusters/taplab/cluster-config/sensitive-file-structures/create_user-registry-dockerconfig-secret.sh
@@ -1,5 +1,5 @@
 kubectl -n tap-install create secret docker-registry \
 --dry-run=client user-registry-dockerconfig \
---docker-server 'https://myrepo-harbor.harbor.svc.cluster.local' \
+--docker-server 'https://myrepo-harbor.akslab1.tap.fynesy.com' \
 --docker-username=admin \
 --docker-password='Harbor12345' -o yaml  > user-registry-dockerconfig.yaml
