4129df49140b0d3642fb43e36e2b85b45f3128b1
Mark Fynes <tfynes@vmware.com>
2023-06-29 16:41:51 -0400

    new ca crt key

--- clusters/taplab/cluster-config/not-deployed/harbortest/gj/create-csr-and-sign.sh
+++ clusters/taplab/cluster-config/not-deployed/harbortest/gj/create-csr-and-sign.sh
@@ -1,12 +1,13 @@
 
+mkdir -p certs2
 #
 # create ca.key
-openssl genrsa -out ca.key 4096
+openssl genrsa -out certs2/ca.key 4096
 # create ca.crt
-openssl req -x509 -new -nodes -sha512 -days 3650 -subj "/C=US/ST=MA/L=Boston/O=Vmware/OU=Tanzu/CN=FynesyCA" -key ca.key -out ca.crt
+openssl req -x509 -new -nodes -sha512 -days 3650 -subj "/C=US/ST=MA/L=Boston/O=Vmware/OU=Tanzu/CN=FynesyCA" -key certs2/ca.key -out certs2/ca.crt
 # openssl 
 # create tls.key
-openssl genrsa -out tls.key 4096
-openssl req -sha512 -new  -subj "/C=US/ST=MA/L=Boston/O=Vmware/OU=Tanzu/CN='myrepo-harbor.akslab.tap.fynesy.com'" -key tls.key -out tls.csr
-openssl x509 -req -sha512 -days 3650 -extfile v3.ext -CA ca.crt -CAkey ca.key -CAcreateserial -in tls.csr -out tls.crt
+openssl genrsa -out certs2/tls.key 4096
+openssl req -sha512 -new  -subj "/C=US/ST=MA/L=Boston/O=Vmware/OU=Tanzu/CN='myrepo-harbor.akslab.tap.fynesy.com'" -key certs2/tls.key -out certs2/tls.csr
+openssl x509 -req -sha512 -days 3650 -extfile v3.ext -CA certs2/ca.crt -CAkey certs2/ca.key -CAcreateserial -in certs2/tls.csr -out certs2/tls.crt
 
