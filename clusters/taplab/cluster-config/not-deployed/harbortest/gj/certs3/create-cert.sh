a12c715aeb6c180f9d56bfe79d045a877439e9a3
Mark Fynes <tfynes@vmware.com>
2023-07-06 11:34:32 -0400

    .

--- clusters/taplab/cluster-config/not-deployed/harbortest/gj/certs3/create-cert.sh
+++ clusters/taplab/cluster-config/not-deployed/harbortest/gj/certs3/create-cert.sh
@@ -1,2 +1,15 @@
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout ./tls.key -x509 -days 365 -out tls.crt
-
+openssl rand -writerand ~/.rnd
+openssl req \
+    -newkey rsa:2048 \
+    -x509 \
+    -nodes \
+    -keyout tls.key \
+    -new \
+    -out tls.crt \
+    -subj /CN=myrepo-harbor.akslab.tap.fynesy.com \
+    -reqexts SAN \
+    -extensions SAN \
+    -config <(cat /System/Library/OpenSSL/openssl.cnf \
+        <(printf '[SAN]\nsubjectAltName=DNS:myrepo-harbor.akslab.tap.fynesy.com')) \
+    -sha256 \
+    -days 3650
--- clusters/taplab/cluster-config/not-deployed/harbortest/gj/certs3/create-k8s-secret.sh
+++ clusters/taplab/cluster-config/not-deployed/harbortest/gj/certs3/create-k8s-secret.sh
@@ -1,1 +1,2 @@
+kubectl -n harbor delete secret harborcerts
 kubectl -n harbor create secret tls harborcerts --cert=tls.crt --key=tls.key
