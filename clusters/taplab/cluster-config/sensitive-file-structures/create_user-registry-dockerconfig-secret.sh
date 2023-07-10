c177e624b4e6fb942545df1b92f267f5a8cfdc62
Mark Fynes <tfynes@vmware.com>
2023-07-10 15:35:49 -0400

    akslab2 domain switch

--- clusters/taplab/cluster-config/sensitive-file-structures/create_user-registry-dockerconfig-secret.sh
+++ clusters/taplab/cluster-config/sensitive-file-structures/create_user-registry-dockerconfig-secret.sh
@@ -1,5 +1,5 @@
 kubectl -n tap-install create secret docker-registry \
 --dry-run=client user-registry-dockerconfig \
---docker-server 'https://myrepo-harbor.akslab1.tap.fynesy.com' \
+--docker-server 'https://myrepo-harbor.akslab2.tap.fynesy.com' \
 --docker-username=admin \
 --docker-password='Harbor12345' -o yaml  > user-registry-dockerconfig.yaml
